e9d5aaca2609dd0b117f1df48eeaa24171b09457
Guillem Gelabert Mestre <ggelabert@hotelbeds.com>
2020-02-25 22:51:30 +0100

    robots.txt

--- bigday/urls.py
+++ bigday/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^', include('wedding.urls')),
     url(r'^', include('guests.urls')),
     url(r'^admin/', admin.site.urls),
     url('^accounts/', include('django.contrib.auth.urls')),
-    url('i18n/', include('django.conf.urls.i18n'))
+    url('i18n/', include('django.conf.urls.i18n')),
+    url(r'^robots\.txt/$', TemplateView.as_view(template_name='robots.txt',content_type='text/plain')),
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt',content_type='text/plain')),
 ]
--- wedding/views.py
+++ wedding/views.py
@@ -1,10 +1,12 @@
 from django.conf import settings
 from django.shortcuts import render
 from django.utils import translation
+from django.contrib.auth.decorators import login_required
 
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
+@login_required
 def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
