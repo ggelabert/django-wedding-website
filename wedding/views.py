0249bcfee4052cebf39f8122fc985573badc5cbf
Guillem Gelabert Mestre <ggelabert@activitiesbank.com>
2020-04-12 16:19:46 +0200

    No login for index

--- wedding/views.py
+++ wedding/views.py
@@ -6,7 +6,6 @@ from django.contrib.auth.decorators import login_required
 from guests.save_the_date import SAVE_THE_DATE_CONTEXT_MAP
 
 
-@login_required
 def home(request):
     return render(request, 'home.html', context={
         'save_the_dates': SAVE_THE_DATE_CONTEXT_MAP,
